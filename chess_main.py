7c15a5cf15053066834f25c70e44ab69c3afacda
Akshar Pandey <pandeyakshar4@gmail.com>
2026-05-02 16:20:58 +0400

    Update chess_main.py
    added an option to start bot to bot game or a bot to human game

--- chess_main.py
+++ chess_main.py
@@ -1,6 +1,7 @@
 import engine as e
 import Game_rule as gr
 import random
+import time as s
 
 turn = random.choice(['W', 'B'])
 
@@ -13,15 +14,18 @@ board = [["BR", "BN", "BB", "BQ", "BK", "BB", "BN", "BR"],
     ["WP", "WP", "WP", "WP", "WP", "WP", "WP", "WP"],
     ["WR", "WN", "WB", "WQ", "WK", "WB", "WN", "WR"]]
 
+bh = input("Enter other opponent (Bot/Human): ")
 counter = 0
-while True:
-    if counter % 2 == 0:
-        turn = 'W'
-    else:
-        turn = 'B'
+try:
+    while True:
+        if counter % 2 == 0:
+            turn = 'W'
+        else:
+            turn = 'B'
 
-    print()
-    if counter % 2 == 0:
+        print()
+        if counter != 0:
+            print("(Half) Move: " + str(counter))
         num = 8
         for z in board:
             print([x if x != 0 else '--' for x in z], end='')
@@ -30,31 +34,100 @@ while True:
         print('  a  ', '  b  ', '  c  ', '  d  ', '  e  ', '  f  ', '  g  ', '  h  ')
         print()
 
-        source = input("Enter source coordinate: ")
-        dest = input("Enter destination coordinate: ")
-        col = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-        source2 = col.index(source[0])
-        source1 = 8 - int(source[1])
-        dest2 = col.index(dest[0])
-        dest1 = 8 - int(dest[1])
 
-        print(source2, source1, dest2, dest1)
-        legal_moves = gr.legal_filter(board, turn)
+        if counter % 2 == 0 and bh == 'B':
+            print("Bot is thinking", end='')
+            move = e.get_best_move(board, 3, turn)
+            print('.', end='')
 
-        if [[source1, source2], [dest1, dest2]] in legal_moves:
-            board[dest1][dest2] = board[source1][source2]
-            board[source1][source2] = 0
+            source, dest = move
+
+            col = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+            row = [1, 2, 3, 4, 5, 6, 7]
+
+            legal_moves = gr.legal_filter(board, turn)
+            if [[source[0], source[1]], [dest[0], dest[1]]] in legal_moves:
+                board[dest[0]][dest[1]] = board[source[0]][source[1]]
+                board[source[0]][source[1]] = 0
+
+            elif not legal_moves:
+                print("Checkmate")
+                break
+
+            else:
+                print("Bot made invalid move, gotta fix that!!!")
+                continue
+
+            s.sleep(0.5)
+            print('.', end='')
+            s.sleep(0.5)
+            print('.')
+
+            print("Bot plays: " + str(board[dest[0]][dest[1]][1]) + col[dest[1]] + str(8 - dest[0]))
+
+        elif counter % 2 == 0 and bh == 'H':
+            source = input("Enter source coordinate (R to resign game): ")
+
+            if source == 'R':
+                print("Game resigned")
+                print(e.minimax(board, 3, turn))
+                break
+
+            dest = input("Enter destination coordinate: ")
+            col = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+            row = [1, 2, 3, 4, 5, 6, 7]
+
+            source2 = col.index(source[0])
+            source1 = 8 - int(source[1])
+            dest2 = col.index(dest[0])
+            dest1 = 8 - int(dest[1])
+
+            legal_moves = gr.legal_filter(board, turn)
+
+            if [[source1, source2], [dest1, dest2]] in legal_moves:
+                board[dest1][dest2] = board[source1][source2]
+                board[source1][source2] = 0
+
+            elif not legal_moves:
+                print("Checkmate")
+                break
+
+            else:
+                print('Invalid Move')
+                continue
 
         else:
-            print('Invalid Move')
-            continue
+            print("Bot is thinking", end='')
+            move = e.get_best_move(board, 3, turn)
+            print('.', end='')
 
-    else:
-        move = e.get_best_move(board, 3, turn)
-        source, dest = move
-        board[dest[0]][dest[1]] = board[source[0]][source[1]]
-        board[source[0]][source[1]] = 0
+            source, dest = move
 
-    counter += 1
+            col = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+            row = [1, 2, 3, 4, 5, 6, 7]
+
+            legal_moves = gr.legal_filter(board, turn)
+            if [[source[0], source[1]], [dest[0], dest[1]]] in legal_moves:
+                board[dest[0]][dest[1]] = board[source[0]][source[1]]
+                board[source[0]][source[1]] = 0
+
+            elif not legal_moves:
+                print("Checkmate")
+                break
+
+            else:
+                print("Bot made invalid move, gotta fix that!!!")
+                continue
+
+            s.sleep(0.5)
+            print('.', end='')
+            s.sleep(0.5)
+            print('.')
+
+            print("Bot plays: " + str(board[dest[0]][dest[1]][1]) + col[dest[1]] + str(8 - dest[0]))
 
 
+        counter += 1
+
+except Exception as e:
+    print("Error: ", e)
